e9f7382b1a1e3d69b1de870de4440ad121f6782f
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2024-04-11 19:32:44 -0500

    Auth service with refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,19 +5,21 @@ import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as argon from 'argon2';
+import { Tokens } from './types';
 
 @Injectable({})
 export class AuthService {
   constructor(
     private prisma: PrismaService,
     private userService: UsersService,
-    private jwt: JwtService,
+    private jwtService: JwtService,
   ) {}
-  signup(facultadId: string, signupDto: SignupDto) {
+
+  signupLocal(facultadId: string, signupDto: SignupDto) {
     return this.userService.createUser(facultadId, signupDto);
   }
 
-  async sigin(signinDto: SigninDto) {
+  async siginLocal(signinDto: SigninDto): Promise<Tokens> {
     // find the user by email
     const user = await this.prisma.user.findUnique({
       where: {
@@ -32,20 +34,61 @@ export class AuthService {
     // if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
     // return this.signToke(user.id, user.email);
-    return this.signToke(user.id, user.email);
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+
+    return tokens;
   }
 
-  async signToke(id: string, email: string): Promise<{ access_token: string }> {
-    const payload = {
+  async logout(userId: string): Promise<boolean> {
+    await this.prisma.user.updateMany({
+      where: {
+        id: userId,
+        hashedRt: {
+          not: null,
+        },
+      },
+      data: {
+        hashedRt: null,
+      },
+    });
+    return true;
+  }
+
+  refreshTokens() {}
+
+  async updateRtHash(id: string, refreshToken: string) {
+    const hash = await argon.hash(refreshToken);
+
+    await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
+  async getTokens(id: string, email: string): Promise<Tokens> {
+    const jwtPayload = {
       sub: id,
       email,
     };
 
-    const token = await this.jwt.signAsync(payload, {
-      expiresIn: '15m',
-      secret: `${process.env.JWT_SECRET}`,
-    });
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        expiresIn: '15m',
+        secret: `${process.env.JWT_SECRET}`,
+      }),
 
-    return { access_token: token };
+      this.jwtService.signAsync(jwtPayload, {
+        expiresIn: '1d',
+        secret: `${process.env.REFRESH_JWT_SECRET}`,
+      }),
+    ]);
+
+    return { access_token: at, refresh_token: rt };
   }
 }
