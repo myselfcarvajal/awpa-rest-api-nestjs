bc15fc80ceae7e8846330c0639f89beee541c0b2
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2024-04-14 18:07:08 -0500

    Unused strategy deleted

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,11 @@ import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import {
-  AccessTokenStrategy,
-  JwtStrategy,
-  RefreshTokenStrategy,
-} from './strategy';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({}), PrismaModule, UsersModule],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    JwtStrategy,
-    AccessTokenStrategy,
-    RefreshTokenStrategy,
-  ],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
 })
 export class AuthModule {}
--- src/auth/strategy/index.ts
+++ src/auth/strategy/index.ts
@@ -1,3 +1,2 @@
-export * from './jwt.strategy';
 export * from './accessToken.strategy';
 export * from './refreshToken.strategy';
