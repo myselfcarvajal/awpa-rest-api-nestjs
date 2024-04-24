57e88f318697a7250fd0068fe23d93bea0a5ebd4
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2024-04-23 19:20:05 -0500

    Using Helmet to secure application

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
