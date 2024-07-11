4988cfc68056fd1b89b53cefa21f7be2cd714a3f
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2024-07-11 10:48:38 -0500

    Adjust role property in signupLocal

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,9 +1,5 @@
-import { Role } from '@prisma/client';
 import {
-  ArrayMaxSize,
-  ArrayUnique,
   IsEmail,
-  IsEnum,
   IsNotEmpty,
   IsNumberString,
   IsString,
@@ -36,15 +32,6 @@ export class SignupDto {
   @IsString()
   @IsNotEmpty()
   apellido: string;
-
-  @IsNotEmpty()
-  @IsEnum(Role, {
-    each: true, // Permitir validación para cada elemento del arreglo
-    message: 'Valid role required',
-  })
-  @ArrayUnique()
-  @ArrayMaxSize(2, { message: 'Maximum two roles allowed' })
-  role: Role[];
 
   // @IsNotEmpty()
   // facultad: { connect: { codigoFacultad: string } };
